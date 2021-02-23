79fb158d52e3f31de7cdd13344c9b892ab86863f
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2021-02-23 21:55:58 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,245 @@
+let sajt1 = document.querySelector("#sajt1");
+let sajt2 = document.querySelector("#sajt2");
+let sajt3 = document.querySelector("#sajt3");
+let sajt4 = document.querySelector("#sajt4");
+let sajt5 = document.querySelector("#sajt5");
+let sajt6 = document.querySelector("#sajt6");
+let sajt7 = document.querySelector("#sajt7");
+let sajt8 = document.querySelector("#sajt8");
+let tekst = document.querySelector("#tekst");
+let tekst2 = document.querySelector("#tekst2");
+let tekst3 = document.querySelector("#tekst3");
+let tekst4 = document.querySelector("#tekst4");
+let tekst5 = document.querySelector("#tekst5");
+let tekst6 = document.querySelector("#tekst6");
+let tekst7 = document.querySelector("#tekst7");
+let tekst8 = document.querySelector("#tekst8");
+let left_arrow = document.querySelector("#left_arrow");
+let right_arrow = document.querySelector("#right_arrow");
+let left_arrow2 = document.querySelector("#left_arrow2");
+let right_arrow2 = document.querySelector("#right_arrow2");
+let left_arrow3 = document.querySelector("#left_arrow3");
+let right_arrow3 = document.querySelector("#right_arrow3");
+let left_arrow4 = document.querySelector("#left_arrow4");
+let right_arrow4 = document.querySelector("#right_arrow4");
+let left_arrow5 = document.querySelector("#left_arrow5");
+let right_arrow5 = document.querySelector("#right_arrow5");
+let left_arrow6 = document.querySelector("#left_arrow6");
+let right_arrow6 = document.querySelector("#right_arrow6");
+let left_arrow7 = document.querySelector("#left_arrow7");
+let right_arrow7 = document.querySelector("#right_arrow7");
+let left_arrow8 = document.querySelector("#left_arrow8");
+let right_arrow8 = document.querySelector("#right_arrow8");
+
+arrow_right.addEventListener("click", function () {
+  arrow_right.style.visibility = "hidden";
+  arrow_left.style.visibility = "hidden";
+  arrow_right2.style.visibility = "visible";
+  arrow_left2.style.visibility = "visible";
+  projekat1.style.visibility = "hidden";
+  projekat2.style.visibility = "visible";
+  sajt1.style.visibility = "hidden";
+  sajt2.style.visibility = "visible";
+  tekst.style.visibility = "hidden";
+  tekst2.style.visibility = "visible";
+})
+
+arrow_left8.addEventListener("click", function () {
+  arrow_right8.style.visibility = "hidden";
+  arrow_left8.style.visibility = "hidden";
+  arrow_right7.style.visibility = "visible";
+  arrow_left7.style.visibility = "visible";
+  projekat8.style.visibility = "hidden";
+  projekat7.style.visibility = "visible";
+  sajt8.style.visibility = "hidden";
+  sajt7.style.visibility = "visible";
+  tekst8.style.visibility = "hidden";
+  tekst7.style.visibility = "visible";
+})
+
+arrow_left7.addEventListener("click", function () {
+  arrow_right7.style.visibility = "hidden";
+  arrow_left7.style.visibility = "hidden";
+  arrow_right6.style.visibility = "visible";
+  arrow_left6.style.visibility = "visible";
+  projekat7.style.visibility = "hidden";
+  projekat6.style.visibility = "visible";
+  sajt7.style.visibility = "hidden";
+  sajt6.style.visibility = "visible";
+  tekst7.style.visibility = "hidden";
+  tekst6.style.visibility = "visible";
+})
+
+arrow_left6.addEventListener("click", function () {
+  arrow_right6.style.visibility = "hidden";
+  arrow_left6.style.visibility = "hidden";
+  arrow_right5.style.visibility = "visible";
+  arrow_left5.style.visibility = "visible";
+  projekat6.style.visibility = "hidden";
+  projekat5.style.visibility = "visible";
+  sajt6.style.visibility = "hidden";
+  sajt5.style.visibility = "visible";
+  tekst6.style.visibility = "hidden";
+  tekst5.style.visibility = "visible";
+})
+
+arrow_left5.addEventListener("click", function () {
+  arrow_right5.style.visibility = "hidden";
+  arrow_left5.style.visibility = "hidden";
+  arrow_right4.style.visibility = "visible";
+  arrow_left4.style.visibility = "visible";
+  projekat5.style.visibility = "hidden";
+  projekat4.style.visibility = "visible";
+  sajt5.style.visibility = "hidden";
+  sajt4.style.visibility = "visible";
+  tekst5.style.visibility = "hidden";
+  tekst4.style.visibility = "visible";
+})
+
+arrow_left4.addEventListener("click", function () {
+  arrow_right4.style.visibility = "hidden";
+  arrow_left4.style.visibility = "hidden";
+  arrow_right3.style.visibility = "visible";
+  arrow_left3.style.visibility = "visible";
+  projekat4.style.visibility = "hidden";
+  projekat3.style.visibility = "visible";
+  sajt4.style.visibility = "hidden";
+  sajt3.style.visibility = "visible";
+  tekst4.style.visibility = "hidden";
+  tekst3.style.visibility = "visible";
+})
+
+arrow_left3.addEventListener("click", function () {
+  arrow_right3.style.visibility = "hidden";
+  arrow_left3.style.visibility = "hidden";
+  arrow_right2.style.visibility = "visible";
+  arrow_left2.style.visibility = "visible";
+  projekat3.style.visibility = "hidden";
+  projekat2.style.visibility = "visible";
+  sajt3.style.visibility = "hidden";
+  sajt2.style.visibility = "visible";
+  tekst3.style.visibility = "hidden";
+  tekst2.style.visibility = "visible";
+})
+
+arrow_left2.addEventListener("click", function () {
+  arrow_right2.style.visibility = "hidden";
+  arrow_left2.style.visibility = "hidden";
+  arrow_right.style.visibility = "visible";
+  arrow_left.style.visibility = "visible";
+  projekat2.style.visibility = "hidden";
+  projekat1.style.visibility = "visible";
+  sajt2.style.visibility = "hidden";
+  sajt1.style.visibility = "visible";
+  tekst2.style.visibility = "hidden";
+  tekst.style.visibility = "visible";
+})
+
+arrow_left.addEventListener("click", function () {
+  arrow_right.style.visibility = "hidden";
+  arrow_left.style.visibility = "hidden";
+  arrow_right8.style.visibility = "visible";
+  arrow_left8.style.visibility = "visible";
+  projekat1.style.visibility = "hidden";
+  projekat8.style.visibility = "visible";
+  sajt1.style.visibility = "hidden";
+  sajt8.style.visibility = "visible";
+  tekst.style.visibility = "hidden";
+  tekst8.style.visibility = "visible";
+})
+
+arrow_right2.addEventListener("click", function () {
+  arrow_right2.style.visibility = "hidden";
+  arrow_left2.style.visibility = "hidden";
+  arrow_right3.style.visibility = "visible";
+  arrow_left3.style.visibility = "visible";
+  projekat2.style.visibility = "hidden";
+  projekat3.style.visibility = "visible";
+  sajt2.style.visibility = "hidden";
+  sajt3.style.visibility = "visible";
+  tekst2.style.visibility = "hidden";
+  tekst3.style.visibility = "visible";
+})
+
+arrow_right3.addEventListener("click", function () {
+  arrow_right3.style.visibility = "hidden";
+  arrow_left3.style.visibility = "hidden";
+  arrow_right4.style.visibility = "visible";
+  arrow_left4.style.visibility = "visible";
+  projekat3.style.visibility = "hidden";
+  projekat4.style.visibility = "visible";
+  sajt3.style.visibility = "hidden";
+  sajt4.style.visibility = "visible";
+  tekst3.style.visibility = "hidden";
+  tekst4.style.visibility = "visible";
+})
+
+arrow_right4.addEventListener("click", function () {
+  arrow_right4.style.visibility = "hidden";
+  arrow_left4.style.visibility = "hidden";
+  arrow_right5.style.visibility = "visible";
+  arrow_left5.style.visibility = "visible";
+  projekat4.style.visibility = "hidden";
+  projekat5.style.visibility = "visible";
+  sajt4.style.visibility = "hidden";
+  sajt5.style.visibility = "visible";
+  tekst4.style.visibility = "hidden";
+  tekst5.style.visibility = "visible";
+})
+
+arrow_right5.addEventListener("click", function () {
+  arrow_right5.style.visibility = "hidden";
+  arrow_left5.style.visibility = "hidden";
+  arrow_right6.style.visibility = "visible";
+  arrow_left6.style.visibility = "visible";
+  projekat5.style.visibility = "hidden";
+  projekat6.style.visibility = "visible";
+  sajt5.style.visibility = "hidden";
+  sajt6.style.visibility = "visible";
+  tekst5.style.visibility = "hidden";
+  tekst6.style.visibility = "visible";
+})
+
+arrow_right6.addEventListener("click", function () {
+  arrow_right6.style.visibility = "hidden";
+  arrow_left6.style.visibility = "hidden";
+  arrow_right7.style.visibility = "visible";
+  arrow_left7.style.visibility = "visible";
+ projekat6.style.visibility = "hidden";
+  projekat7.style.visibility = "visible";
+  sajt6.style.visibility = "hidden";
+  sajt7.style.visibility = "visible";
+  tekst6.style.visibility = "hidden";
+  tekst7.style.visibility = "visible";
+})
+
+arrow_right7.addEventListener("click", function () {
+  arrow_right7.style.visibility = "hidden";
+  arrow_left7.style.visibility = "hidden";
+  arrow_right8.style.visibility = "visible";
+  arrow_left8.style.visibility = "visible";
+  projekat7.style.visibility = "hidden";
+  projekat8.style.visibility = "visible";
+  sajt7.style.visibility = "hidden";
+  sajt8.style.visibility = "visible";
+  tekst7.style.visibility = "hidden";
+  tekst8.style.visibility = "visible";
+})
+
+arrow_right8.addEventListener("click", function () {
+  arrow_right8.style.visibility = "hidden";
+  arrow_left8.style.visibility = "hidden";
+  arrow_right.style.visibility = "visible";
+  arrow_left.style.visibility = "visible";
+  projekat8.style.visibility = "hidden";
+  projekat1.style.visibility = "visible";
+  sajt8.style.visibility = "hidden";
+  sajt1.style.visibility = "visible";
+  tekst8.style.visibility = "hidden";
+  tekst.style.visibility = "visible";
+})
+
+
+
+
+
